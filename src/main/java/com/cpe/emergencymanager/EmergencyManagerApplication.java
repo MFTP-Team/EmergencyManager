04ae0bde57849ff73c86fa5ffd96a8c28505b7c1
Martin <martin.jourjon42@gmail.com>
2022-12-06 17:19:35 +0100

    feat: Init du projet

---
+++ src/main/java/com/cpe/emergencymanager/EmergencyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cpe.emergencymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpe/emergencymanager/EmergencyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpe.emergencymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
