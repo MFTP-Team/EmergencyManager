44c26c8a034c87f5b9623c2ba98caab704e298c6
Francois Cheval <orionpegase7@gmail.com>
2023-01-12 09:46:42 +0100

    ta mere

--- src/main/java/com/cpe/emergencymanager/repository/TruckRepository.java
+++ src/main/java/com/cpe/emergencymanager/repository/TruckRepository.java
@@ -1,6 +1,5 @@
 package com.cpe.emergencymanager.repository;
 
-import com.cpe.emergencymanager.model.FiremanEntity;
 import com.cpe.emergencymanager.model.TruckEntity;
 import org.springframework.data.repository.Repository;
 
@@ -9,7 +8,7 @@ import java.util.List;
 /**
  * Classe Repository permettant d'accéder aux objets Truck en bases
  */
-public interface TruckRepository extends Repository<FiremanEntity, Integer> {
+public interface TruckRepository extends Repository<TruckEntity, Integer> {
     TruckEntity findById(Integer id);
     TruckEntity save(TruckEntity truckEntity);
     TruckEntity deleteById(Integer id);
